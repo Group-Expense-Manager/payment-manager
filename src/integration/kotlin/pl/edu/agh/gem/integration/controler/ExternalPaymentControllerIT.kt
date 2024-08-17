50f96e479173976319e20cfc8a4adb126655e2e2
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-08-17 18:25:09 +0200

    GEM-116 | added missing test

--- src/integration/kotlin/pl/edu/agh/gem/integration/controler/ExternalPaymentControllerIT.kt
+++ src/integration/kotlin/pl/edu/agh/gem/integration/controler/ExternalPaymentControllerIT.kt
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.HttpStatus.FORBIDDEN
 import pl.edu.agh.gem.assertion.shouldHaveHttpStatus
 import pl.edu.agh.gem.assertion.shouldHaveValidationError
+import pl.edu.agh.gem.assertion.shouldHaveValidatorError
 import pl.edu.agh.gem.helper.group.DummyGroup.GROUP_ID
 import pl.edu.agh.gem.helper.user.DummyUser.OTHER_USER_ID
 import pl.edu.agh.gem.helper.user.DummyUser.USER_ID
@@ -16,6 +17,8 @@ import pl.edu.agh.gem.integration.ability.stubAttachmentStoreGenerateBlankAttach
 import pl.edu.agh.gem.integration.ability.stubCurrencyManagerAvailableCurrencies
 import pl.edu.agh.gem.integration.ability.stubCurrencyManagerExchangeRate
 import pl.edu.agh.gem.integration.ability.stubGroupManagerGroupData
+import pl.edu.agh.gem.internal.service.Quadruple
+import pl.edu.agh.gem.util.DummyData.ANOTHER_USER_ID
 import pl.edu.agh.gem.util.DummyData.CURRENCY_1
 import pl.edu.agh.gem.util.DummyData.CURRENCY_2
 import pl.edu.agh.gem.util.DummyData.EXCHANGE_RATE_VALUE
@@ -28,11 +31,17 @@ import pl.edu.agh.gem.util.createGroupResponse
 import pl.edu.agh.gem.util.createMembersDTO
 import pl.edu.agh.gem.util.createPaymentCreationRequest
 import pl.edu.agh.gem.validation.ValidationMessage.ATTACHMENT_ID_NULL_OR_NOT_BLANK
+import pl.edu.agh.gem.validation.ValidationMessage.BASE_CURRENCY_EQUAL_TO_TARGET_CURRENCY
+import pl.edu.agh.gem.validation.ValidationMessage.BASE_CURRENCY_NOT_AVAILABLE
 import pl.edu.agh.gem.validation.ValidationMessage.BASE_CURRENCY_NOT_BLANK
+import pl.edu.agh.gem.validation.ValidationMessage.BASE_CURRENCY_NOT_IN_GROUP_CURRENCIES
 import pl.edu.agh.gem.validation.ValidationMessage.BASE_CURRENCY_PATTERN
 import pl.edu.agh.gem.validation.ValidationMessage.MESSAGE_NULL_OR_NOT_BLANK
 import pl.edu.agh.gem.validation.ValidationMessage.POSITIVE_AMOUNT
 import pl.edu.agh.gem.validation.ValidationMessage.RECIPIENT_ID_NOT_BLANK
+import pl.edu.agh.gem.validation.ValidationMessage.RECIPIENT_IS_CREATOR
+import pl.edu.agh.gem.validation.ValidationMessage.RECIPIENT_NOT_GROUP_MEMBER
+import pl.edu.agh.gem.validation.ValidationMessage.TARGET_CURRENCY_NOT_IN_GROUP_CURRENCIES
 import pl.edu.agh.gem.validation.ValidationMessage.TARGET_CURRENCY_PATTERN
 import pl.edu.agh.gem.validation.ValidationMessage.TITLE_MAX_LENGTH
 import pl.edu.agh.gem.validation.ValidationMessage.TITLE_NOT_BLANK
@@ -81,7 +90,7 @@ class ExternalPaymentControllerIT(
         response shouldHaveHttpStatus CREATED
     }
 
-    should("not create payment when user dont have access") {
+    should("return forbidden when user dont have access") {
         // given
         val user = createGemUser()
         val groupId = GROUP_ID
@@ -95,7 +104,7 @@ class ExternalPaymentControllerIT(
         response shouldHaveHttpStatus FORBIDDEN
     }
 
-    context("return validation exception cause:") {
+    context("return bad request with cause:") {
         withData(
             nameFn = { it.first },
             Pair(TITLE_NOT_BLANK, createPaymentCreationRequest(title = "")),
@@ -118,6 +127,59 @@ class ExternalPaymentControllerIT(
             // then
             response shouldHaveHttpStatus BAD_REQUEST
             response shouldHaveValidationError expectedMessage
+        }
+    }
+    context("return forbidden  cause validator exception:") {
+        withData(
+            nameFn = { it.first },
+            Quadruple(
+                RECIPIENT_IS_CREATOR,
+                createPaymentCreationRequest(recipientId = USER_ID),
+                arrayOf(CURRENCY_1, CURRENCY_2),
+                arrayOf(CURRENCY_1, CURRENCY_2),
+            ),
+            Quadruple(
+                RECIPIENT_NOT_GROUP_MEMBER,
+                createPaymentCreationRequest(recipientId = ANOTHER_USER_ID),
+                arrayOf(CURRENCY_1, CURRENCY_2),
+                arrayOf(CURRENCY_1, CURRENCY_2),
+            ),
+            Quadruple(
+                BASE_CURRENCY_NOT_IN_GROUP_CURRENCIES,
+                createPaymentCreationRequest(targetCurrency = null),
+                arrayOf(),
+                arrayOf(CURRENCY_1, CURRENCY_2),
+            ),
+            Quadruple(
+                BASE_CURRENCY_EQUAL_TO_TARGET_CURRENCY,
+                createPaymentCreationRequest(amount = createAmountDto(currency = CURRENCY_1), targetCurrency = CURRENCY_1),
+                arrayOf(CURRENCY_1, CURRENCY_2),
+                arrayOf(CURRENCY_1, CURRENCY_2),
+            ),
+            Quadruple(
+                TARGET_CURRENCY_NOT_IN_GROUP_CURRENCIES,
+                createPaymentCreationRequest(),
+                arrayOf(CURRENCY_1),
+                arrayOf(CURRENCY_1, CURRENCY_2),
+            ),
+            Quadruple(BASE_CURRENCY_NOT_AVAILABLE, createPaymentCreationRequest(), arrayOf(CURRENCY_1, CURRENCY_2), arrayOf(CURRENCY_2)),
+
+        ) { (expectedMessage, paymentCreationRequest, groupCurrencies, availableCurrencies) ->
+            // given
+            stubCurrencyManagerAvailableCurrencies(createCurrenciesResponse(*availableCurrencies))
+            stubGroupManagerGroupData(createGroupResponse(groupCurrencies = createCurrenciesDTO(*groupCurrencies)), GROUP_ID)
+            stubCurrencyManagerExchangeRate(
+                createExchangeRateResponse(value = EXCHANGE_RATE_VALUE),
+                CURRENCY_1,
+                CURRENCY_2,
+            )
+
+            // when
+            val response = service.createPayment(paymentCreationRequest, createGemUser(), GROUP_ID)
+
+            // then
+            response shouldHaveHttpStatus BAD_REQUEST
+            response shouldHaveValidatorError expectedMessage
         }
     }
 },)
